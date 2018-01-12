990a64d2129e5bd51094e1b8b472eb811bf49e75
Tine Steurs <tine@ptr-d3350kezopmve58a87.18120a2.ip6.access.telenet.be>
2018-01-12 13:26:09 +0100

    Convert to MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,88 +9,107 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demorgon"]
-    
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
-
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy D"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
             itemArray = items
         }
-       
     }
-
+    
+    
     //MARK - Tableview Datasource Methods
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        //value is the cell.accessorytype. We set it to 
+        
+        cell.accessoryType = item.done  ? .checkmark : .none
         return cell
-        
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemArray.count
+        
     }
     
     //MARK - TableView Delegate Methods
     
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       // print(itemArray[indexPath.row])
+       
         
-
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark  {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        //Hieronder: Heel onelegant. Bovenstaand: ! betekent "tegenovergesteld"
+        // if itemArray[indexPath.row].done == false {
+        //    itemArray[indexPath.row].done = true
+       // } else {
+           
+       //     itemArray[indexPath.row].done = false
+       //}
         
-        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        
         
     }
     
     //MARK - Add New Items
     
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
         
-        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add button on our UIAlert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
             self.tableView.reloadData()//reload met nieuwe data
         }
-        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
-            
         }
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
         
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
     
-
 }
+
 
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tine Steurs on 12/01/2018.
+//  Copyright © 2018 TinSte. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
+
+
